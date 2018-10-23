40fb738cbd22f39f06653caa161aadaf50b19217
Elhadji Barry <elhadji.a.barry@desjardins.com>
2018-10-23 18:42:34 -0400

    7.125 Create Pet Type, Pet and Visit entities. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Elhadji Barry (dur5268)
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
